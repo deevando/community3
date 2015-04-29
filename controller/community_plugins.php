4841c4855e06d4a9e2df926211aa4c90524b4291
carlos <carlos@m5a97>
2015-04-29 18:48:38 +0200

    Añadido soporte para añadir plugins al listado de la comunidad.

---
+++ controller/community_plugins.php
@@ -0,0 +1,162 @@
+<?php
+
+/*
+ * This file is part of FacturaSctipts
+ * Copyright (C) 2015  Francesc Pineda Segarra  shawe.ewahs@gmail.com
+ * Copyright (C) 2015  Carlos García Gómez      neorazorx@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_model('comm3_plugin.php');
+
+class community_plugins extends fs_controller
+{
+   public $plugin;
+   public $lista_plugins;
+   public $mis_plugins;
+   public $editar_plugin;
+   public $ver_plugin;
+   
+   public function __construct()
+   {
+      parent::__construct(__CLASS__, 'Plugins', 'comunidad', FALSE, TRUE);
+   }
+   
+   /**
+    * Parte privada de la página.
+    */
+   protected function private_core()
+   {
+      $this->editar_plugin = FALSE;
+      $this->ver_plugin = FALSE;
+      
+      $this->plugin = new comm3_plugin();
+      
+      /* Modificar un elemento existente */
+      if( isset($_POST['id']) )
+      {
+         $this->editar_plugin = $this->plugin->get($_POST['id']);
+         
+         if ( $this->editar_plugin )
+         {
+            $this->editar_plugin->descripcion            = $_POST[ 'descripcion' ];
+            $this->editar_plugin->link                   = $_POST[ 'link' ];
+            $this->editar_plugin->estable                = $_POST[ 'estable' ];
+            $this->editar_plugin->version                = $_POST[ 'version' ];
+            $this->editar_plugin->ultima_modificacion    = $_POST[ 'ultima_modificacion' ];
+
+            if ( $this->editar_plugin->save() )
+            {
+               $this->new_message( "Se han modificado los datos del plugin." );
+
+               $this->editar_plugin = $this->plugin->get( $_POST[ 'id' ] );
+            }
+            else
+            {
+               $this->new_error_msg( "Ha ocurrido un error modificando el plugin." );
+            }
+         }
+         else
+         {
+            $this->new_error_msg( "Ha ocurrido un error accidiendo el plugin." );
+         }
+      }
+      /* Insertamos elemento nuevo */
+      else if ( isset( $_POST[ 'nombre' ] ) )
+      {
+         $this->plugin->nick                 = $this->user->nick;
+         $this->plugin->nombre               = $_POST[ 'nombre' ];
+         $this->plugin->descripcion          = $_POST[ 'descripcion' ];
+         $this->plugin->link                 = $_POST[ 'link' ];
+         $this->plugin->estable              = ($_POST[ 'estable' ] == 'TRUE');
+         $this->plugin->version              = intval($_POST[ 'version' ]);
+         $this->plugin->ultima_modificacion  = $_POST[ 'ultima_modificacion' ];
+         $this->plugin->descargas            = 0;
+         
+         if( strlen( trim($this->plugin->nombre) ) < 2 )
+         {
+            $this->new_error_msg( "Debes usar un nombre de más de 2 caracteres." );
+         }
+         else if( $this->plugin->save() )
+         {
+            $this->new_message( "Se ha insertado el plugin correctamente." );
+         }
+         else
+         {
+            $this->new_error_msg( "Ha ocurrido un error guardando el plugin." );
+         }
+      }
+      /* Obtenemos elemento recibido */
+      else if ( isset( $_GET[ 'id' ] ) )
+      {
+         if ( $this->user->nick == $this->plugin->get( $_GET[ 'id' ] )->nick )
+         {
+            $this->editar_plugin = $this->plugin->get( $_GET[ 'id' ] );
+         }
+         else
+         {
+            $this->ver_plugin = $this->plugin->get( $_GET[ 'id' ] );
+         }
+      }
+      /* Eliminamos un elemento existente */
+      else if ( isset( $_GET[ 'delete' ] ) )
+      {
+         $delete_plugin = $this->plugin->get( $_GET[ 'delete' ] );
+
+         if ( $delete_plugin )
+         {
+            if ( $delete_plugin->delete() )
+            {
+               $this->new_message( 'Se ha eliminado el plugin correctamente.' );
+            }
+            else
+            {
+               $this->new_error_msg( 'Ha ocurrido un error eliminando el plugin.' );
+            }
+         }
+         else
+         {
+            $this->new_error_msg( 'Plugin no encontrado.' );
+         }
+      }
+
+      /* Mostraremos siempre la lista */
+      $this->lista_plugins = $this->plugin->all();
+      $this->mis_plugins = $this->plugin->all_by_dev($this->user->nick);
+   }
+   
+   protected function public_core()
+   {
+      $this->template = FALSE;
+      
+      if( isset($_GET['json']) )
+      {
+         $plugin = new comm3_plugin();
+         
+         header('Content-Type: application/json');
+         echo json_encode( $plugin->all() );
+      }
+   }
+   
+   public function path()
+   {
+      if( defined('COMM3_PATH') )
+      {
+         return COMM3_PATH;
+      }
+      else
+         return '';
+   }
+}
---
+++ model/comm3_plugin.php
@@ -0,0 +1,177 @@
+<?php
+
+/*
+ * This file is part of FacturaSctipts
+ * Copyright (C) 2015  Francesc Pineda Segarra  shawe.ewahs@gmail.com
+ * Copyright (C) 2015  Carlos García Gómez      neorazorx@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class comm3_plugin extends fs_model
+{
+   public $id;
+   public $nick;
+   public $nombre;
+   public $descripcion;
+   public $link;
+   public $estable;
+   public $version;
+   public $ultima_modificacion;
+   public $descargas;
+   
+   public function __construct($v = FALSE)
+   {
+      parent::__construct('comm3_plugins', 'plugins/community3/');
+      if($v)
+      {
+         $this->id = $this->intval($v['id']);
+         $this->nick = $v['nick'];
+         $this->nombre = $v['nombre'];
+         $this->descripcion = $v['descripcion'];
+         $this->link = $v['link'];
+         $this->estable = $this->str2bool($v['estable']);
+         $this->version = intval($v['version']);
+         $this->ultima_modificacion = date('d-m-Y', strtotime($v['ultima_modificacion']));
+         $this->descargas = intval($v['descargas']);
+      }
+      else
+      {
+         $this->id = NULL;
+         $this->nick = NULL;
+         $this->nombre = NULL;
+         $this->descripcion = NULL;
+         $this->changelog = NULL;
+         $this->link = NULL;
+         $this->estable = FALSE;
+         $this->version = 1;
+         $this->ultima_modificacion = date('d-m-Y');
+         $this->descargas = 0;
+      }
+   }
+   
+   protected function install()
+   {
+      return '';
+   }
+   
+   public function url()
+   {
+      if( is_null($this->id) )
+      {
+         return 'index.php?page=community_plugins';
+      }
+      else
+      {
+         return 'index.php?page=community_plugins&id='.$this->id;
+      }
+   }
+   
+   public function get($id)
+   {
+      $data = $this->db->select("SELECT * FROM ". $this->table_name ." WHERE id = ".$this->var2str($id).";");
+      if($data)
+      {
+         return new comm3_plugin($data[0]);
+      }
+      else
+      {
+         return FALSE;
+      }
+   }
+   
+   public function exists()
+   {
+      if( is_null($this->id) )
+      {
+         return FALSE;
+      }
+      else
+      {
+         return $this->db->select("SELECT * FROM ". $this->table_name ." WHERE id = ".$this->var2str($this->id).";");
+      }
+   }
+   
+   public function save()
+   {
+      if( $this->exists() )
+      {
+         $sql = "UPDATE ".$this->table_name." SET  nick = ".$this->var2str($this->nick).
+                 ", nombre = ".$this->var2str($this->nombre).
+                 ", descripcion = ".$this->var2str($this->descripcion).
+                 ", link = ".$this->var2str($this->link).
+                 ", estable = ".$this->var2str($this->estable).
+                 ", version = ".$this->var2str($this->version).
+                 ", ultima_modificacion = ".$this->var2str($this->ultima_modificacion).
+                 ",  descargas = ".$this->var2str($this->descargas).
+                 " WHERE id = ".$this->var2str($this->id).";";
+         
+         return $this->db->exec($sql);
+      }
+      else
+      {
+         $sql = "INSERT INTO ".$this->table_name." (nick,nombre,descripcion,link,estable,version,ultima_modificacion,descargas) VALUES (".
+                 $this->var2str($this->nick).",".$this->var2str($this->nombre).",".$this->var2str($this->descripcion).",".
+                 $this->var2str($this->link).",".$this->var2str($this->estable).",".$this->var2str($this->version).",".
+                 $this->var2str($this->ultima_modificacion).",".$this->var2str($this->descargas).");";
+         
+         if( $this->db->exec($sql) )
+         {
+            $this->id = $this->db->lastval();
+            return TRUE;
+         }
+         else
+         {
+            return FALSE;
+         }
+      }
+   }
+   
+   public function delete()
+   {
+      return $this->db->exec("DELETE FROM ". $this->table_name ." WHERE id = ".$this->var2str($this->id).";");
+   }
+   
+   public function all()
+   {
+      $vlist = array();
+      
+      $data = $this->db->select("SELECT * FROM ". $this->table_name ." ORDER BY nombre ASC, nick ASC;");
+      if($data)
+      {
+         foreach($data as $d)
+         {
+            $vlist[] = new comm3_plugin($d);
+         }
+      }
+      
+      return $vlist;
+   }
+   
+   public function all_by_dev($nick)
+   {
+      $vlist = array();
+      
+      $data = $this->db->select("SELECT * FROM ".$this->table_name." WHERE nick = ".$this->var2str($nick)." ORDER BY nombre ASC;");
+      if($data)
+      {
+         foreach($data as $d)
+         {
+            $vlist[] = new comm3_plugin($d);
+         }
+      }
+      
+      return $vlist;
+   }
+}
