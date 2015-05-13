c6874cf126ca8196c67e1ead7a5dcd576c8d9083
carlos <carlos@m5a97>
2015-05-13 20:57:05 +0200

    Pequeña corrección.

--- model/comm3_item.php
+++ model/comm3_item.php
@@ -558,6 +558,7 @@ class comm3_item extends fs_model
       if($nick)
       {
          $sql .= " AND (ultimo_comentario != ".$this->var2str($nick)." OR ultimo_comentario IS NULL)";
+         $sql .= " AND nick != ".$this->var2str($nick);
       }
       if(!$admin)
       {
